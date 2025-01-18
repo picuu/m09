c93d4b5feee19fe8ca6120d74b39c99fc64a59f8
picuu <carloscapoforcada@gmail.com>
2025-01-18 20:07:58 +0100

    add scene preview videos when hover to movie thumbnail

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 function init() {
-  showMovieBannerAfterTime(5)
+  const TIME_TO_SHOW_BANNER = 5
+  const TIME_TO_HIDE_VIDEO = 5
+
+  showMovieBannerAfterTime(TIME_TO_SHOW_BANNER)
+
+  const scenePreviewVideos = document.querySelectorAll('.scene-preview')
+  scenePreviewVideos.forEach(video => listenVideoHover(video, TIME_TO_HIDE_VIDEO))
 }
 
-function showMovieBannerAfterTime(seconds) {
+function showMovieBannerAfterTime (seconds) {
   setTimeout(() => {
     const bannerElement = document.querySelector('#movie-banner')
     bannerElement.classList.remove('hidden')
@@ -13,6 +19,40 @@ function showMovieBannerAfterTime(seconds) {
       videoElement.classList.add('hidden')
     }, 250)
   }, seconds * 1000)
+}
+
+function listenVideoHover (video, timeToHide = 5) {
+  const previewCardParent = video.parentElement.parentElement
+  let videoPassedTimeToHide = false
+
+  const hideScenePreview = () => {
+    video.pause()
+    video.classList.add('hidden')
+  }
+
+  const handleTimeUpdate = () => {
+    if (video.currentTime >= timeToHide && !videoPassedTimeToHide) {
+      hideScenePreview()
+      videoPassedTimeToHide = true
+    }
+  }
+
+  const handleMouseEnter = async () => {
+    videoPassedTimeToHide = false
+    video.currentTime = 0
+    await video.play()
+
+    video.addEventListener('timeupdate', handleTimeUpdate)
+  }
+
+  const handleMouseLeave = () => {
+    video.pause()
+    video.classList.remove('hidden')
+    video.removeEventListener('timeupdate', handleTimeUpdate)
+  }
+
+  previewCardParent.addEventListener('mouseenter', handleMouseEnter)
+  previewCardParent.addEventListener('mouseleave', handleMouseLeave)
 }
 
 init()
